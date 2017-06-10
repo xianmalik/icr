3057accaa096eb4d042a53aede6d52a19392e99a
Zubayer <haider15-5902@diu.edu.bd>
2017-06-10 20:09:56 +0600

    Responsive left

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 jQuery('document').ready(function($){
-
+    $('.block-reveal').wrapInner("<span></span>").append("<div class='block-over'></div>");
 });
+
 jQuery(window).load(function(){
-    jQuery('body').addClass('loaded').find('.page-wrap').css('opacity', 1);
+    jQuery('body').addClass('loaded')
+    .find('.page-wrap').css('opacity', 1)
+    .delay(1000).queue(function(){
+        $(this).find('#Slider').addClass('animate');
+    });
+});
+
+$(window).scroll(function(){
+    $('section:not(.animate)').each(function(){
+        if( $(window).scrollTop() - $(window).height() * .5  >= $(this).position().top){
+            $(this).addClass('animate');
+        }
+    });
 });
